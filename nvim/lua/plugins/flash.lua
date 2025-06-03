e791e9f3262d8c2dda5cdab0593147780c406c66
Jakub Leško <mail@jakublesko.com>
2025-06-03 10:42:49 +0200

    chore(nvim): remove unused plugins

--- nvim/lua/plugins/flash.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {},
-    keys = {
-        { "s", function() require("flash").jump() end,       desc = "Flash" },
-        { "S", function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    },
-}
