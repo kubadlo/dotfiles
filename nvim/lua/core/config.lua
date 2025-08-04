f465c804733117527edf108776a4b93030ae8b8f
Jakub Leško <mail@jakublesko.com>
2025-08-04 13:21:06 +0200

    feat(nvim): use one global statusline

--- nvim/lua/core/config.lua
+++ nvim/lua/core/config.lua
@@ -4,14 +4,9 @@ vim.opt.relativenumber = true
 
 -- Make <Tab> 4 spaces wide
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.smartindent = true
-
--- Make splits more consistent
-vim.opt.splitbelow = true
-vim.opt.splitright = true
 
 -- Set intendation based on file defaults
 vim.opt.smartindent = true
@@ -28,15 +23,13 @@ vim.opt.incsearch = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+-- Make splits more consistent
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
 -- Start scrolling 5 lines before window edge
 vim.opt.scrolloff = 5
 vim.opt.sidescrolloff = 5
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
 
 -- Show current file name in terminal title
 vim.opt.title = true
@@ -46,6 +39,9 @@ vim.opt.confirm = true
 
 -- Hide mode since it's visible in statusbar
 vim.opt.showmode = false
+
+-- Use one global status bar per nvim instance
+vim.opt.laststatus = 3
 
 -- Show diagnostics inline
 vim.diagnostic.config({
