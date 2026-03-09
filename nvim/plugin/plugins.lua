cf726b484dd4aab2c44586b79e44aa4d0c0c4632
Jakub Leško <mail@jakublesko.com>
2026-03-09 19:49:48 +0100

    feat(nvim): auto show documentation on completion

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -77,6 +77,9 @@ later(function()
 
     require("blink.cmp").setup({
         completion = {
+            documentation = {
+                auto_show = true,
+            },
             ghost_text = {
                 enabled = true,
             },
