6ab9126960f6d9e655f44c4609f81654a817f944
Jakub Leško <mail@jakublesko.com>
2026-03-11 16:03:41 +0100

    feat(nvim): increase max memory for vtsls

---
+++ nvim/after/lsp/vtsls.lua
@@ -0,0 +1,10 @@
+---@type vim.lsp.Config
+return {
+    settings = {
+        typescript = {
+            tsserver = {
+                maxTsServerMemory = 8192,
+            },
+        },
+    },
+}
