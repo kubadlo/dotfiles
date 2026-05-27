83f324fb6bdd7f10f7cefd1230075a25de26528c
Jakub Leško <mail@jakublesko.com>
2026-05-27 08:17:04 +0200

    feat(nvim): replace vtsls with tsgo

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -4,19 +4,6 @@ vim.pack.add({
     "https://github.com/neovim/nvim-lspconfig",
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(event)
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if (client == nil) then
-            return
-        end
-
-        if client:supports_method("inlayHint/resolve") then
-            vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
-        end
-    end
-})
-
 require("mason").setup({
     install_root_dir = vim.fn.stdpath("data") .. "/site/mason",
 })
@@ -34,7 +21,7 @@ require("mason-lspconfig").setup({
         "lua_ls",
         "rust_analyzer",
         "tailwindcss",
-        "vtsls",
+        "tsgo",
         "yamlls",
     },
 })
